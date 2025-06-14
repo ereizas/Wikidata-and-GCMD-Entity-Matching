e8d7046c71782ffb599d6c5802e8f3689135ffd1
Eric Reizas <tuo17224@temple.edu>
2025-06-14 17:49:19 -0400

    Extract search results for term

--- extract.py
+++ extract.py
@@ -2,6 +2,9 @@ import requests
 from json import dump
 
 def write_all_gcmd_ents_to_json():
+    """
+    Queries the GCMD API using pagination to acquire all of GCMD's entities and their definitions/descriptions
+    """
     page = 1
     url = "https://gcmd.earthdata.nasa.gov/kms/concepts/concept_scheme/sciencekeywords?format=json&page_size=2000&page_num="
     num_results = 0
@@ -16,7 +19,6 @@ def write_all_gcmd_ents_to_json():
     filtered_data = dict()
     num_entities = 0
     while num_entities<num_results:
-        print("here")
         if response.status_code==200:
             data = response.json()
             concepts = data.get("concepts")
@@ -33,5 +35,23 @@ def write_all_gcmd_ents_to_json():
     file = open("gcmd_ents.json","w")
     dump(filtered_data,file)
     
-write_all_gcmd_ents_to_json()
+def get_wikidata_search_results(term:str):
+    """
+    Queries Wikidata's search engine with a term and returns the search results
+
+    @param term:phrase to search for
+    """
+    filtered_data = dict()
+    response = requests.get(f"https://www.wikidata.org/w/api.php?action=wbsearchentities&search={term}&language=en&format=json&limit=10")
+    data = None
+    if response.status_code==200:
+        data = response.json()
+    else:
+        return {"Error occurred":f"{response.status_code}"}
+    for res in data["search"]:
+        filtered_data[res["id"]] = {"term":res["display"]["label"]["value"],"description":res["description"],"match":{"alias":res["match"]["type"],"text":res["match"]["text"]}}
+    return filtered_data
+
+#write_all_gcmd_ents_to_json()
+print(get_wikidata_search_results("conservation"))
     
