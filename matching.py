7b749c3e4c9bebed680af273cb97b39a6fb38b54
Eric Reizas <tuo17224@temple.edu>
2025-07-19 09:32:07 -0400

    Focus single target entity and its candidates for ranking matchings

--- matching.py
+++ matching.py
@@ -33,24 +33,23 @@ def n_gram_vectorize(target:str,candidates:list[str]):
     tfidf_matrix = vectorizer.fit_transform(texts)
     return tfidf_matrix[0], tfidf_matrix[1:]
 
-def get_best_match(gcmd_ents:dict, wikidata_search_res:dict, target_uuid:str, rank_fxn, inverse:bool=False):
+def get_best_match(target:str, wikidata_search_res:dict, rank_fxn, inverse:bool=False):
     """
     Gets the best match for the target entity from the search results based on the rank function
 
-    @param gcmd_ents : dictionary of all GCMD entities
-    @param wikidata_search_res : search results on WikiData for each GCMD entity
-    @param target_uuid : uuid of target entity to match from GCMD
+    @param target : GCMD entity to match
+    @param wikidata_search_res : search results on WikiData for the target GCMD entity
     @param rank_fxn : function used for ranking the candidates' match to target
     @param inverse : boolean indicating whether a lower score means better match (inverse order)
     """
     max_score = float("-inf")
     best_match = ""
-    for res in wikidata_search_res[target_uuid]:
-        score = rank_fxn(gcmd_ents[target_uuid]["term"],wikidata_search_res[target_uuid][res]["term"])
+    for res in wikidata_search_res:
+        score = rank_fxn(target,wikidata_search_res[res]["term"])
         if inverse:
             score = -score
         if score>max_score:
-            print(f"Score: {score} Term: {wikidata_search_res[target_uuid][res]["term"]}")
+            print(f"Score: {score} Term: {wikidata_search_res[res]["term"]}")
             max_score = score
             best_match = res
     return best_match
@@ -58,6 +57,14 @@ def get_best_match(gcmd_ents:dict, wikidata_search_res:dict, target_uuid:str, ra
 if __name__=="__main__":
     file = open("gcmd_ents.json","r")
     gcmd_ents = load(file)
+    file.close()
     file = open("gcmd_ents_wikidata_search_res.json","r")
     wikidata_search_res = load(file)
-    print(get_best_match(gcmd_ents, wikidata_search_res, "b6fd22ab-dca7-4dfa-8812-913453b5695b", edit_distance, True))
+    file.close()
+    file = open("gcmd_ent_wikidata_ent_matching_ground_truth.json","r")
+    ground_truth = load(file)
+    file.close()
+    for uuid in gcmd_ents:
+        if ground_truth[uuid]:
+            print(get_best_match(gcmd_ents[uuid]["term"], wikidata_search_res[uuid], edit_distance, True))
+            break
