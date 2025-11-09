0f9e2fdcff69bfa98b21ccbdf2d1a7507bf9a090
Eric Reizas <tuo17224@temple.edu>
2025-11-09 12:54:24 -0500

    collect results for different thresholds

---
+++ graphing.py
@@ -0,0 +1,64 @@
+import json
+from collections import defaultdict
+from config import google_cloud_api_key
+from matching import (
+    build_unique_text_reprs,
+    batch_embeddings_with_cache,
+    rank_by_edit_dist,
+    rank_by_n_gram,
+    rank_by_embedding,
+    update_stats
+)
+
+if __name__=="__main__":
+    file = open("gcmd_ents.json","r")
+    gcmd_ents = json.load(file)
+    file.close()
+    file = open("search_res.json","r")
+    wikidata_search_res = json.load(file)
+    file.close()
+    file = open("ground_truth.json","r")
+    ground_truth = json.load(file)
+    file.close()
+    thresholds = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+    print(thresholds)
+    # adjust as needed
+    LABELED_SAMPLES = 475
+    # test out different thresholds
+    """text_sources, all_texts = build_unique_text_reprs(gcmd_ents, wikidata_search_res, LABELED_SAMPLES)
+    all_texts = list(all_texts)
+    embeddings = batch_embeddings_with_cache(all_texts, api_key=google_cloud_api_key, db_path="embeddings_cache_old.db")
+    text_to_emb = dict(zip(list(all_texts), embeddings))
+    gcmd_embeddings = {}
+    candidate_embeddings = defaultdict(dict)
+    for text, usages in text_sources.items():
+        for info in usages:
+            if info[0] == "gcmd":
+                gcmd_embeddings[info[1]] = text_to_emb[text]
+            elif info[0] == "candidate":
+                candidate_embeddings[info[1]][info[2]] = text_to_emb[text]"""
+    edit_dist_stats = [{"tp":0, "fp":0, "tn":0, "fn":0} for t in thresholds]
+    edit_dist_res = {}
+    n_gram_stats = [{"tp":0, "fp":0, "tn":0, "fn":0} for t in thresholds]
+    n_gram_res = {}
+    """embedding_stats = [{"tp":0, "fp":0, "tn":0, "fn":0} for t in thresholds]
+    embedding_res = {}"""
+    num_samples = 0
+    for uuid in gcmd_ents:
+        if wikidata_search_res[uuid]:
+            ground_truth_matches = ground_truth[uuid].split(",")
+            edit_dist_res[uuid] = rank_by_edit_dist(gcmd_ents[uuid]["term"], wikidata_search_res[uuid], threshold=2)
+            n_gram_res[uuid] = rank_by_n_gram(gcmd_ents[uuid],wikidata_search_res[uuid], threshold=0)
+            """embedding_res[uuid] = [item[0] for item in rank_by_embedding(gcmd_embeddings[uuid],
+                                               list(candidate_embeddings[uuid].values()),
+                                               list(candidate_embeddings[uuid].keys()))]"""
+            for i in range(len(thresholds)):
+                edit_dist_res[uuid] = [item for item in edit_dist_res[uuid] if item[1]<=thresholds[len(thresholds)-i-1]]
+                update_stats(edit_dist_res[uuid][0][0] if edit_dist_res[uuid] else None, ground_truth_matches, edit_dist_stats[i])
+                n_gram_res[uuid] = [item for item in n_gram_res[uuid] if item[1]>=thresholds[i]]
+                update_stats(n_gram_res[uuid][0][0] if n_gram_res[uuid] else None, ground_truth_matches, n_gram_stats[i])
+                """embedding_res[uuid] = [item for item in embedding_res[uuid] if item[1]>=thresholds[i]]
+                update_stats(embedding_res[uuid][0][0] if embedding_res[uuid] else None, ground_truth_matches, embedding_stats[i])"""
+        num_samples+=1
+        if num_samples==LABELED_SAMPLES:
+            break
